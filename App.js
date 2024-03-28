614b4b13cb18a61ad70cc3a163381a4fdf90add0
andreascastello <andreas.castello06@gmail.com>
2024-03-28 10:21:33 +0100

    init email

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useState} from "react";
+import {signup} from "./auth_signup_password";
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>Email</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={setEmail}
+            value={email}
+        ></TextInput>
+        <Text>Password</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={setPassword}
+            value={password}
+            secureTextEntry={true}
+        ></TextInput>
+        <Button title="Sign Up !" onPress={() => signup(email, password)}></Button>
+      </View>
   );
 }
 
@@ -16,5 +32,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAGwxdkLtGmjwF_h8I8Wo8XqUD0kQpxPH0",
+    authDomain: "web-cloud-ynov-cb72d.firebaseapp.com",
+    projectId: "web-cloud-ynov-cb72d",
+    storageBucket: "web-cloud-ynov-cb72d.appspot.com",
+    messagingSenderId: "1019075798173",
+    appId: "1:1019075798173:web:beb508387b113881852074"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
